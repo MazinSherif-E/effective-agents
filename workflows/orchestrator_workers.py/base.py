43c5b56f445cafa69fe994ef91262b70a1b9c4d0
MazinSherif-E <mazin.sherif100@gmail.com>
2025-01-31 11:30:33 +0200

    corrected typo in folder name

--- workflows/orchestrator_workers.py/base.py
+++
@@ -1,7 +0,0 @@
-from llm_factory.openai_provider import OpenAIProvider
-
-class BaseOrchestratorWorker:
-    def __init__(self, agent: OpenAIProvider, max_workers: int = 3):
-        self.agent = agent
-        self.max_workers = max_workers
-        self.results = {}
